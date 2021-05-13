dc0fc4d405ae116d1e76f1ba62edfbd83bf141f0
AtanasiusIvannoel <50091059+darkPigeonn@users.noreply.github.com>
2021-05-13 21:15:24 +0700

    bab2 kelar

---
+++ app/src/main/java/com/example/panduanpraktikum/KiKd.java
@@ -0,0 +1,14 @@
+package com.example.panduanpraktikum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KiKd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ki_kd);
+    }
+}
